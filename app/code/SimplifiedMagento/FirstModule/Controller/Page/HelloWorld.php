fa2deb24a5701e198ce39c63d70e371ee0c27ed7
Mudasser Hayat <alhayat.dev@gmail.com>
2021-03-28 02:12:30 +0500

    Proxy example.

--- app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -9,7 +9,9 @@ namespace SimplifiedMagento\FirstModule\Controller\Page;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\Request\Http;
 use SimplifiedMagento\FirstModule\Api\PencilInterface;
+use SimplifiedMagento\FirstModule\Model\HeavyService;
 use SimplifiedMagento\FirstModule\Model\PencilFactory;
 
 class HelloWorld implements ActionInterface
@@ -23,19 +25,38 @@ class HelloWorld implements ActionInterface
      * @var PencilFactory $pencilFactory
      */
     protected PencilFactory $pencilFactory;
+    /**
+     * @var Http
+     */
+    private Http $http;
+    /**
+     * @var HeavyService
+     */
+    private HeavyService $heavyService;
 
     public function __construct(
         PencilFactory $pencilFactory,
         PencilInterface $pencilInterface,
-        ProductRepositoryInterface $productRepository
+        ProductRepositoryInterface $productRepository,
+        Http $http,
+        HeavyService $heavyService
     ) {
         $this->pencilInterface = $pencilInterface;
         $this->productRepository = $productRepository;
         $this->pencilFactory = $pencilFactory;
+        $this->http = $http;
+        $this->heavyService = $heavyService;
     }
 
     public function execute()
     {
+        $id = $this->http->getParam('id', 0);
+        if ($id == 1) {
+            echo $this->heavyService->printHeavyServiceMessage();
+        } else {
+            echo "Heavy service not used." . PHP_EOL;
+        }
+        exit();
         /*
         $pencil = $this->pencilFactory->create(['name' => 'Mudasser Hayat', 'school' => 'Intermediate']);
         var_dump($pencil);
@@ -62,6 +83,6 @@ class HelloWorld implements ActionInterface
         var_dump($student);
         */
 
-        echo "Main Function" . PHP_EOL;
+//        echo "Main Function" . PHP_EOL;
     }
 }
---
+++ app/code/SimplifiedMagento/FirstModule/Model/HeavyService.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * PLEASE ENTER ONE LINE SHORT DESCRIPTION OF CLASS
+ *
+ * Class HeavyService
+ * @package SimplifiedMagento\FirstModule\Model
+ */
+
+
+namespace SimplifiedMagento\FirstModule\Model;
+
+class HeavyService
+{
+
+}
